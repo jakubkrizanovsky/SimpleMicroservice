cacf86e506c28eb105b842c83156c9477730aa4c
Jakub Krizanovsky <jakub.krizanovsky@gmail.com>
2026-01-12 16:30:13 +0100

    Base services improvements

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,12 +2,25 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    message = f'Hello!'
+@app.route('/message/<name>')
+def hello(name: str):
+    message = f'Hello {name}!'
     response = f'{{ "message": "{message}" }}\r\n'
     code = 200
     return response, code,  {'Content-Type': 'application/json'}
+
+@app.route('/secret-message/<name>')
+def secret(name: str):
+    response:dict = {'message': None}
+    code:int = -1
+    if name.lower() == 'jakub':
+        response['message'] = 'Super secret message!'
+        code = 200
+    else:
+        code = 418
+        
+    return response, code,  {'Content-Type': 'application/json'}
+    
 
 @app.route("/healthcheck")
 def healthcheck():
--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,18 +1,34 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from requests.exceptions import ConnectionError, Timeout
 import requests
 
 app = Flask(__name__)
 
 be_base_url = "http://backend:5000"
 
-@app.route('/')
-def get_message():
-    be_response = requests.get(be_base_url + "/hello")
-    print(f"Received response: {be_response.json()}")
-    if be_response.status_code == 200:
-        return be_response.json()["message"], 200,  {'Content-Type': 'application/json'}
-    else:
-        return "error", 500
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    message = None
+    secret = None
+
+    if request.method == 'POST':
+        name = request.form.get('username', '')
+
+        if name:
+            try:
+                response = requests.get(f"{be_base_url}/message/{name}")
+                if response.status_code == 200:
+                    message = response.json()['message']
+
+                response = requests.get(f"{be_base_url}/secret-message/{name}")
+                if response.status_code == 200:
+                    secret = response.json()['message']
+
+            except (ConnectionError, Timeout):
+                return render_template('error.html'), 503
+
+    return render_template('index.html', message=message, secret=secret)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
